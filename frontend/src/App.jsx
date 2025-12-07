077d0673bf39b1b2cef942fa06db28bce00f835a
vvn-sequeira <svivianmarcel@gmail.com>
2025-12-07 18:45:13 +0530

    Project Structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
 import './App.css'
+import Sidebar from './Sidebar'
+import ChatWindow from './ChatWindow'
+import Chat from './Chat'
+import { Mycontext } from './MyContext'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const ProviderValues = {};
 
   return (
-    <>
-
-    </>
+    <div className='app'>
+      <Mycontext.Provider value={ProviderValues}>
+          <Sidebar></Sidebar>
+          <ChatWindow></ChatWindow>
+      </Mycontext.Provider>
+    </div>
   )
 }
 
---
+++ frontend/src/ChatWindow.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import "./ChatWindo.css"
+export default function ChatWindow() {
+  return (
+    <div>ChatWindow</div>
+  )
+}
